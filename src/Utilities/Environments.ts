22775779630d5936766f51ce5ceb67bc85d02775
Jonathan Cernero <cernero.jon@gmail.com>
2024-02-08 11:26:10 -0500

    testing environments for deployment

--- src/Utilities/Environments.ts
+++ src/Utilities/Environments.ts
@@ -1,8 +1,11 @@
 let APIURL = '';
 
 switch (window.location.hostname) {
-  case 'localhost' || '127.0.0.1':
-    APIURL = 'http://localhost:3005/api/v1';
+  // case 'localhost' || '127.0.0.1':
+  //   APIURL = 'http://localhost:3005/api/v1';
+  //   break;
+  case 'https://diligence-server.onrender.com':
+    APIURL = 'https://diligence-server.onrender.com';
     break;
 }
 

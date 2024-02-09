c99813a2c5074d4fe7cff9b8d6b76407ce2059c1
Jonathan Cernero <cernero.jon@gmail.com>
2024-02-09 08:13:29 -0500

    updating environment variable for deployment

--- src/Utilities/Environments.ts
+++ src/Utilities/Environments.ts
@@ -1,12 +1,12 @@
-let APIURL = '';
+let APIURL = 'https://diligence-server.onrender.com/api/v1';
 
 switch (window.location.hostname) {
+  case 'https://diligence-server.onrender.com':
+    APIURL = 'https://diligence-server.onrender.com/api/v1';
+    break;
   // case 'localhost' || '127.0.0.1':
   //   APIURL = 'http://localhost:3005/api/v1';
   //   break;
-  case 'https://diligence-server.onrender.com':
-    APIURL = 'https://diligence-server.onrender.com';
-    break;
 }
 
 export default APIURL;

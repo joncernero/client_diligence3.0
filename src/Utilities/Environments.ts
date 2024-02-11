d8ba54992103c8ad31b04b8328aa7994a1308dce
Jonathan Cernero <cernero.jon@gmail.com>
2024-02-11 09:42:19 -0500

    Updating environment variables and app name

--- src/Utilities/Environments.ts
+++ src/Utilities/Environments.ts
@@ -1,12 +1,12 @@
 let APIURL = 'https://diligence-server.onrender.com/api/v1';
 
 switch (window.location.hostname) {
-  case 'https://diligence-server.onrender.com':
+  case 'https://diligencedemo.vercel.app/':
     APIURL = 'https://diligence-server.onrender.com/api/v1';
     break;
-  // case 'localhost' || '127.0.0.1':
-  //   APIURL = 'http://localhost:3005/api/v1';
-  //   break;
+  case 'localhost' || '127.0.0.1':
+    APIURL = 'http://localhost:3005/api/v1';
+    break;
 }
 
 export default APIURL;
